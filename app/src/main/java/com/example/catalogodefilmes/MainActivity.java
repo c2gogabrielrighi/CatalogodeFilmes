8e64ef5ee57cb99222f86e869d8dedadcd3ae5ff
Gabriel Righi <50993290+c2gogabrielrighi@users.noreply.github.com>
2019-11-11 15:44:33 -0300

    Added icons.

--- app/src/main/java/com/example/catalogodefilmes/MainActivity.java
+++ app/src/main/java/com/example/catalogodefilmes/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
     Button btnPopular;
